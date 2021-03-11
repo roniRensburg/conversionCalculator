fe6369fd541e8528c2208abed4630e96c7a4387e
roni rensburg <ronirensburg@gmail.com>
2021-03-11 10:28:24 +0200

    Updates

---
+++ precache-manifest.f3344dccfddd024f849248bf380dce23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03ed47128c1d7694552512152e47695",
+    "url": "/conversionCalculator/index.html"
+  },
+  {
+    "revision": "7644eea4d7a783075e1c",
+    "url": "/conversionCalculator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "85807eb7157d26097258",
+    "url": "/conversionCalculator/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "7644eea4d7a783075e1c",
+    "url": "/conversionCalculator/static/js/2.37a120f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/conversionCalculator/static/js/2.37a120f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85807eb7157d26097258",
+    "url": "/conversionCalculator/static/js/main.57d64fb5.chunk.js"
+  },
+  {
+    "revision": "53e88306da46e60d42d4",
+    "url": "/conversionCalculator/static/js/runtime-main.1dd5713a.js"
+  }
+]);
